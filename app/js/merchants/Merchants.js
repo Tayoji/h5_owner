9b9f9b5561d9d59e57e92330ae9afa9add8f06aa
tangyoujian <tangyoujian@xiucheren.net>
2016-12-16 09:29:45 +0800

    ＃增加＃商家列表筛选功能

--- app/js/merchants/Merchants.js
+++ app/js/merchants/Merchants.js
@@ -13,6 +13,7 @@ import SearchBar from '../Tools/SearchBar'
 import ReactIScroll from 'reactjs-iscroll';
 import iScroll from 'iscroll/build/iscroll-probe';
 import MerchantsItem from './MerchantsItem';
+import MerchantsSelector from './MerchantsSelector';
 // import '../../style/Merchants.scss';
 require('../../style/Merchants.scss');
 var Api = require('../Tools/Api')
@@ -21,24 +22,34 @@ export default class Merchants extends React.Component {
     super(props);
     this.loadData.bind(this);
     this.handleChange.bind(this);
+
     this.state = {
       keyword: '',
+      pullUpState:true,
       pageNo: 1,
+      selectorIndex:-1,
       shopList: [],
       address: {
         coords: null,
       },
-      options: [{
+      merchantsoptions: [{
         name: "全部服务",
-        id: 'all'
+        value: 'all',
+        type:'services'
       }, {
         name: "全城",
-        id: '-1'
+        value: '-1',
+        type:'city',
       }, {
         name: "综合排序",
-        id: 'all',
+        value: 'default',
+        type: 'sort',
       }, {
         name: '筛选',
+        value:'',
+        value1:'1',
+        type: 'selector',
+
       }],
       position: {
         point: {
@@ -47,6 +58,16 @@ export default class Merchants extends React.Component {
         }
       }
     }
+    this.parms = {
+      "category": "all",
+      "orderType": "default",
+      "pageNo": 1,
+      "cityCode": 268,
+      "areaCode": -1,
+      "insuranceLevel": 1,
+      "shopType": ""
+    };
+
   }
 
 
@@ -60,7 +81,6 @@ export default class Merchants extends React.Component {
       var geolocation = new BMap.Geolocation();
       geolocation.getCurrentPosition(function (r) {
         if (this.getStatus() == BMAP_STATUS_SUCCESS) {
-          console.log('++++', r);
           self.setState({
             position: r
           });
@@ -77,7 +97,6 @@ export default class Merchants extends React.Component {
 
   componentWillUnmount() {
 
-    console.log(this.props.location.key)
     if (this.state.position.address){
       window.sessionStorage.setItem(this.props.location.key, JSON.stringify(this.state));
     }else {
@@ -85,35 +104,51 @@ export default class Merchants extends React.Component {
     }
   }
 
-  loadData() {
+  loadData(callback) {
     let self = this;
-    var parms = {
-      "category": "all",
-      "orderType": "default",
-      "pageNo": this.state.pageNo,
-      "cityCode": 268,
-      "areaCode": -1,
-      "insuranceLevel": 1,
-      "shopType": ""
-    }
     if (this.state.position) {
-      parms.mapCoordinates = this.state.position.point.lng + "," + this.state.position.point.lat;
+      this.parms.mapCoordinates = this.state.position.point.lng + "," + this.state.position.point.lat;
     }
     if (this.state.keyword) {
-      parms.keyword = this.state.keyword;
+      this.parms.keyword = this.state.keyword;
     }
-    Api.POST(Api.url("/owner/serviceShop/list.jhtml"), parms, (res) => {
-      self.setState({
-        shopList: res.data.shopList,
-      });
-    }, (e) => {
+    Api.POST(Api.url("/owner/serviceShop/list.jhtml"), this.parms, (res) => {
+      if (res.success){
+        var shopList = self.state.shopList;
+        if (self.parms.pageNo == 1){
+          shopList = res.data.shopList;
+        }else {
+          shopList = shopList.concat(res.data.shopList);
+        }
+        self.setState({
+          shopList: shopList,
+          pullUpState:res.data.shopList.length >= 20
+        });
 
+      }
+
+      if (callback){
+        callback();
+      }
+    }, (e) => {
+      if (callback){
+        callback();
+      }
     })
   }
+  handleRefresh(upordown, callback) {
+    if (callback && typeof callback === 'function') {
 
+      if (upordown == 'up') {
+        this.parms.pageNo++;
+      } else {
+        this.parms.pageNo = 1;
+      }
+      this.loadData(callback);
+    }
+  }
   handleChange(e) {
     let self = this;
-    console.log(e);
     if (this.keywordTimer) {
       clearTimeout(this.keywordTimer)
     }
@@ -121,6 +156,7 @@ export default class Merchants extends React.Component {
       self.setState({
         keyword: e
       })
+      self.parms.pageNo = 1;
       self.loadData()
       clearTimeout(self.keywordTimer)
     }, 1000);
@@ -130,19 +166,69 @@ export default class Merchants extends React.Component {
   handleBlur(e) {
 
   }
+  handleSelector(index){
+    if (this.state.selectorIndex == index){
+      this.setState({
+        selectorIndex:-1,
+      })
+    }else {
+      this.setState({
+        selectorIndex:index,
+      })
+    }
+  }
+  handlemerchantsSelector(option){
+    var merchantsoptions = this.state.merchantsoptions;
+    merchantsoptions.map((item)=>{
+      if (item.type == option.type){
+        return option;
+      }else {
+        return item;
+      }
+    })
+    switch (option.type){
+      case "services":
+        this.parms.category = option.value
+        break;
+      case "city":
+        this.parms.areaCode = option.value;
+        break;
+      case "sort":
+        this.parms.orderType = option.value;
+        break
+      case 'selector':
+        this.parms.shopType = option.value;
+        if (option.value == "" || option.value=="traditionShop"){
+          this.parms.insuranceLevel = option.value1;
+        }else {
+          this.parms.insuranceLevel = 1;
+        }
+        break
+    }
+    this.setState({
+      merchantsoptions:merchantsoptions,
+      selectorIndex:-1,
+    })
+    this.loadData();
 
+  }
   render() {
-    let {position} = this.state;
+    let {position,selectorIndex,merchantsoptions,pullUpState} = this.state;
     return (<Container>
       <div className="merchants-header">
         <SearchBar height="44px" onChange={this.handleChange.bind(this)}/>
+        <MerchantsSelector  option = {selectorIndex != -1 ? merchantsoptions[selectorIndex] : null} onSelected = {
+          this.handlemerchantsSelector.bind(this)
+        }/>
         <div className="merchants-options">
           {
-            this.state.options.map((item, index) => {
+            merchantsoptions.map((item, index) => {
               return (<div className="merchants-option" style={{borderColor: (index == 3 ? "#fff" : null)}} key={index}>
-                <div>
+                <div className={`merchants-options-${index == selectorIndex ? "selected" : 'normal'}`}
+                      onClick={this.handleSelector.bind(this,index)}
+                >
                   <p>{item.name}</p>
-                  <img src="../i/down.png"/>
+                  <img src={`${index == selectorIndex ? "../i/up.png" : "../i/down.png"}`} />
                 </div>
               </div>)
             })
@@ -155,6 +241,8 @@ export default class Merchants extends React.Component {
         <ReactIScroll
           iScroll={iScroll}
           ref="merchantsrefresh"
+          pullUp={pullUpState}
+          handleRefresh={this.handleRefresh.bind(this)}
         >
           <div className="merchants-address">
             <div>
@@ -180,8 +268,6 @@ export default class Merchants extends React.Component {
           </ul>
         </ReactIScroll>
       </div>
-
-
     </Container>);
   }
 
--- app/js/merchants/MerchantsSelector.js
+++ app/js/merchants/MerchantsSelector.js
@@ -1,3 +1,258 @@
 /**
  * Created by xiucheren on 16/12/15.
  */
+import React from 'react';
+import '../../style/merchantsselector.scss';
+var Api = require('../Tools/Api');
+//服务
+const services = [{
+  name: '',
+  values: [{name: "全部服务", value: "all"}, {name: "洗车", value: "xiche"}, {name: "保养", value: "baoyang"},
+    {name: "维修", value: "weixiu"}, {name: "钣金喷漆", value: "banjinpenqi"}, {name: "美容装饰", value: "meirong"},
+    {name: "轮胎轮毂", value: "luntai"}, {name: "加装改装", value: "jiazhuanggaizhuang"}, {name: "道路救援", value: "jiuyuan"}]
+}];
+
+//排序
+const sorts = [{
+  name: '',
+  values: [{name: "综合排序", value: "default"}, {name: "距我最近", value: "distance"}, {name: "好评优先", value: "review"}]
+}];
+
+//筛选
+const selectors = [{
+  name: '商家类型',
+  values: [
+    {name: "全部商家", value: ""}, {name: "汽修站", value: "traditionShop"}, {
+      name: "配件专营店",
+      value: "partShop"
+    }, {name: "分时租赁站", value: "rentShop"}
+  ]
+}, {
+  name: '维修保障金',
+  values: [{name: "不限", value: "1"}, {name: "¥1000", value: "1"}, {name: "¥2000", value: "2"},
+    {name: "¥5000", value: "3"}, {name: "¥20000", value: "4"},
+    {name: "¥50000", value: "5"}, {
+      name: "¥200000", value: "7"
+    }]
+}]
+
+export default class MerchantsSelector extends React.Component {
+  constructor(props) {
+    super(props);
+    this.loadBaiduList.bind(this);
+    this.state = {
+      data:selectors,
+      indexpaths:[0,0],
+      option:null,
+    }
+  }
+  componentWillReceiveProps(prors){
+    let prorsoOption = prors.option;
+    let stateOption = this.state.option;
+    var indexpaths = this.state.indexpaths;
+    if (prorsoOption){
+      let data = null;
+      $('.merchants-selector-body').css('display','block');
+      switch (prorsoOption.type){
+        case "services":
+          for (var index = 0;index<services[0].values.length;index++){
+            var item = services[0].values[index];
+            if (prorsoOption.value == item.value){
+              indexpaths[0] = index;
+              break;
+            }
+          }
+
+          data = services;
+          break;
+        case "city":
+          this.loadBaiduList({cityCode:268},prorsoOption);
+          break;
+        case "sort":
+          for (var index = 0;index<sorts[0].values.length;index++){
+            var item = sorts[0].values[index];
+            if (prorsoOption.value == item.value){
+              indexpaths[0] = index;
+              break;
+            }
+          }
+          data = sorts;
+          break
+        case 'selector':
+          for (var x = 0;x<selectors.length;x++){
+            var items = selectors[x].values;
+            for (var y = 0;y<items.length;y++){
+              var item = items[y];
+
+              if (item.value == prorsoOption.value || prorsoOption.value1 == item.value){
+                indexpaths[x] = y;
+              }
+            }
+          }
+          data= selectors;
+          break
+      }
+      if (data){
+        this.setState({
+          option:prorsoOption,
+          data:data,
+          indexpaths:indexpaths
+        })
+      }else {
+        this.setState({
+          option:prorsoOption,
+        })
+      }
+      $('.merchants-selector-body-con').fadeIn(500)
+
+      // if (stateOption && stateOption.type == prorsoOption.type){
+      // }
+
+    }else{
+      $('.merchants-selector-body-con').fadeOut(500,(e)=>{
+        this.setState({
+          data:null,
+          option:null
+        })
+        $('.merchants-selector-body').css('display','none')
+      })
+    }
+  }
+  componentDidMount(){
+    // this.loadBaiduList({cityCode:268});
+    $('.merchants-selector-body').css('display','none');
+  }
+  loadBaiduList(p,option){
+    var indexPaths = this.state.indexpaths;
+
+    let listLocalStorage = window.localStorage.getItem(JSON.stringify(p));
+    if (listLocalStorage){
+
+      let list = JSON.parse(listLocalStorage);
+      for (var index = 0;index<list.length;index++){
+        var item = list[index];
+        if (option.value == item.value){
+          indexPaths[0] = index;
+          break;
+        }
+      }
+      this.setState({
+        data:[{name:'',values:JSON.parse(listLocalStorage)}],
+        indexpaths:indexPaths,
+        option:option
+      })
+      return;
+    }
+    let self = this;
+
+    Api.GET(Api.url('/common/area/baiduList.jhtml'),p,(res)=>{
+      if (res.success){
+        let list = res.data.areaList.map((item,index)=>{
+          if (item.value == self.props.option.value){
+            indexPaths[0] = index;
+          }
+          return {name:item.name,value:item.id};
+        })
+
+        window.localStorage.setItem(JSON.stringify(p),JSON.stringify(list));
+        self.setState({
+          data:[{name:'',values:list}],
+          indexpaths:indexPaths,
+          option:option
+
+
+        })
+      }else {
+
+      }
+    },(e)=>{
+
+    });
+  }
+  handleClickOptionInput(i,j){
+    var {indexpaths,option,data} = this.state;
+    indexpaths[i] = j;
+    this.setState({
+      indexpaths:indexpaths
+    })
+
+    if(option && option.type != 'selector'){
+      $('.merchants-selector-body-con').fadeOut(500,(e)=>{
+        this.setState({
+          data:null,
+          option:null
+        })
+        option.name = data[0].values[indexpaths[0]].name;
+        option.value = data[0].values[indexpaths[0]].value;
+        if (this.props.onSelected){
+          this.props.onSelected(option);
+        }
+        $('.merchants-selector-body').css('display','none')
+
+      })
+    }
+  }
+  render() {
+    const {data,option,indexpaths} = this.state;
+    return (<div className="merchants-selector-body">
+      <div className="merchants-selector-body-con">
+        <div className="merchants-selector-body-options">
+          {
+            data ?
+            data.map((item,i)=>{
+              return (<div className="merchants-selector-body-options-item" key={i}>
+                {
+                  item.name == "" ?  null : <p>{item.name}</p>
+                }
+                <div>
+                  {
+                    item.values.map((button,j)=>{
+                      var selected = false;
+                      for (var x = 0;x<indexpaths.length;x++){
+                        if (x == i&&indexpaths[x]==j){
+                          selected = true;
+                          break;
+                        }
+                      }
+                      return (<input value={button.name} disabled={( i == 1 &&  indexpaths[0] > 1)} className={`merchants-selector-button-${  selected ? "selected":"normal"}`} type="button" key={j} onClick={
+                        this.handleClickOptionInput.bind(this,i,j)
+                      }/>);
+                    })
+                  }
+                </div>
+              </div>);
+            }) : null
+          }
+        </div>
+        {
+          option && option.type == 'selector' ? (<div>
+            <p className="merchants-selector-note">注:维修保障金是修车人网为使用我平台服务的车主做维修担保特设的专用资金，主要用于承担应维修厂维修能力或修车人提供商品原因造成的二次维修费用</p>
+            <div className="merchants-selector-buttons">
+              <button className="chongzhi-button" onClick={(e)=>{
+                this.setState({
+                  indexpaths:[0,0]
+                })
+              }}>重置</button>
+              <div></div>
+              <button className="true-button" onClick={(e)=>{
+                $('.merchants-selector-body-con').fadeOut(500,(e)=>{
+                  this.setState({
+                    data:null,
+                    option:null
+                  })
+                  option.value = data[0].values[indexpaths[0]].value;
+                  option.value1 = data[1].values[indexpaths[1]].value;
+                  if (this.props.onSelected){
+                    this.props.onSelected(option);
+                  }
+                  $('.merchants-selector-body').css('display','none')
+
+                })
+              }}>确定</button>
+            </div>
+          </div>) : null
+        }
+      </div>
+    </div>);
+  }
+}
